31fb5a0551721120553acc37aa0c3ae81aa52d05
thethao <66192167+thaophamhetxat@users.noreply.github.com>
2021-11-19 23:24:53 +0700

    start

---
+++ src/main/java/thethao/day3mapstrucanddtos/Day3MapstrucAndDtOsApplication.java
@@ -0,0 +1,13 @@
+package thethao.day3mapstrucanddtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3MapstrucAndDtOsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3MapstrucAndDtOsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thethao/day3mapstrucanddtos/Day3MapstrucAndDtOsApplicationTests.java
@@ -0,0 +1,13 @@
+package thethao.day3mapstrucanddtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3MapstrucAndDtOsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
